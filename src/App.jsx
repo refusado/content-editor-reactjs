80d16ecec273b4a0781b8647945dcaafba7dfc7c
Renan <orefusado@gmail.com>
2023-07-06 22:33:23 -0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+      <div className="bg-white w-[600px] min-h-[200px] mx-auto rounded-xl border shadow-md border-black/20">
+      </div>
+    </div>
+  )
+}
